30f57eaeefa321b32631f062fe9107b9cbe7adb6
Martin Trapp <94928215+martrapp@users.noreply.github.com>
2025-02-23 20:56:12 +0100

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,37 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import viewTransitions from 'astro-vtbot/starlight-view-transitions'
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import {
+  viewTransitions,
+  remarkEndOfMarkdown,
+} from "astro-vtbot/starlight-view-transitions";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			plugins: [viewTransitions()],
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  site: "https://vtbot-starlight-demo.pages.dev",
+  markdown: {
+    remarkPlugins: [remarkEndOfMarkdown],
+  },
+  integrations: [
+    starlight({
+      title: "My Docs",
+      plugins: [viewTransitions()],
+      social: {
+        github: "https://github.com/withastro/starlight",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
 });
